043235e09801601cbb634ba0a772b39ec7d0220d
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-10-08 15:39:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Percipio Instant Search - Visual Studio Code Extension
+This extension adds a quick command to search Percipio without leaving your favorite VS Code. You can find the command by name `percipio search` or by using the hotkeys `cmd+h` on Mac or `ctrl+h` on Windows, `h` stands for `help`. As an alternative you can execute search instantly from a context menu (select text -> right click -> Percipio Search) or by using the hotkeys `cmd+shift+h` on Mac or `ctrl+shift+h` on Windows.
+
+## Features
+- Percipio search results open in your default browser
+- Execute search with a search term preview (ctrl/cmd+h) or instantly (ctrl/cmd+shift+h)
+
+## Release Notes
+This is an initial version of the Percipio Instant Search extension.
+
+## Acknowledgements
+This was inspired by the great work of Alexey Strakh on the [Stackoverflow Extension](https://github.com/alexeystrakh/vscode-stackoverflow-extension)
+
+## Tags
+`percipio`, `skillsoft`, `question`, `answer`, `search`, `instant search`
---
+++ src/extension.ts
@@ -0,0 +1,143 @@
+import * as vscode from 'vscode';
+import { URL } from 'url';
+
+import open = require('open');
+
+export function activate(context: vscode.ExtensionContext) {
+  /**
+   * Checks the urlstring is a valid Percipio URL
+   * it must be https:// and must end .percipio.com
+   *
+   * @param {string} [urlstring]
+   * @returns {boolean}
+   */
+  const isValidPercipioUrl = (urlstring?: string): boolean => {
+    if (!urlstring) {
+      return false;
+    }
+    let parsedurl;
+
+    try {
+      parsedurl = new URL(urlstring);
+    } catch (_) {
+      return false;
+    }
+
+    // Check
+    return (parsedurl.protocol === 'https:' && parsedurl.hostname.toLowerCase().endsWith('.percipio.com'));
+  };
+
+  /**
+   * Gets the selected text from the active editor
+   *
+   * @returns {string}
+   */
+  const getSelectedText = (): string => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return '';
+    }
+
+    const { document } = editor;
+    const eol = document.eol === 1 ? '\n' : '\r\n';
+
+    const selectedTextLines = editor.selections.map((selection) => {
+      if (
+        selection.start.line === selection.end.line
+        && selection.start.character === selection.end.character
+      ) {
+        const { range } = document.lineAt(selection.start);
+        const text = editor.document.getText(range);
+        return `${text}${eol}`;
+      }
+
+      return editor.document.getText(selection);
+    });
+
+    let result = '';
+    if (selectedTextLines.length > 0) {
+      result = selectedTextLines[0].trim();
+    }
+
+    return result;
+  };
+
+  /**
+   * Get the current language of the editor
+   *
+   * @returns {string}
+   */
+  const getLanguageId = (): string => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return '';
+    }
+    const { document } = editor;
+    return document.languageId;
+  };
+
+  /**
+   * Opens a new browser window to the Percipio site with the searchterm
+   *
+   * @param {string} searchTerm
+   * @returns {void}
+   */
+  const executeSearch = (searchTerm: string): void => {
+    if (!searchTerm || searchTerm.trim() === '') {
+      return;
+    }
+
+    const currentSite = vscode.workspace.getConfiguration('percipioSearch').get<string>('siteUrl');
+    if (!currentSite || currentSite.trim() === '') {
+      return;
+    }
+
+    const language = getLanguageId();
+    const search = language !== '' ? `"${searchTerm.trim()}" ${language}` : searchTerm.trim();
+    const encodedWebSearchTerm = encodeURIComponent(search);
+    const percipioSearchUrl = `${currentSite}/search?q=${encodedWebSearchTerm}`;
+
+    open(percipioSearchUrl);
+  };
+
+  const searchBySelection = vscode.commands.registerCommand(
+    'extension.percipio-search-selection',
+    async () => {
+      const searchTerm = getSelectedText();
+      await executeSearch(searchTerm);
+    },
+  );
+
+  const searchWithPrompt = vscode.commands.registerCommand(
+    'extension.percipio-search',
+    async () => {
+      const selectedText = getSelectedText();
+      const searchTerm = await vscode.window.showInputBox({
+        ignoreFocusOut: selectedText === '',
+        placeHolder: 'Enter your Percipio search query',
+        // prompt: 'search for tooltip',
+        value: selectedText,
+        valueSelection: [0, selectedText.length + 1],
+      });
+      if (searchTerm) {
+        await executeSearch(searchTerm);
+      }
+    },
+  );
+
+  const configureSearch = vscode.commands.registerCommand('config.commands.configure-percipio-search', async () => {
+    const currentSite = vscode.workspace.getConfiguration('percipioSearch').get<string>('siteUrl');
+    // 1) Getting the Site Url
+    const percipioSite = await vscode.window.showInputBox({ placeHolder: 'Enter the Percipio Site URL', value: currentSite });
+
+    // Validate the url
+    if (percipioSite && isValidPercipioUrl(percipioSite) && currentSite !== percipioSite) {
+      // 2) Update if different
+      vscode.workspace.getConfiguration('percipioSearch').update('siteUrl', percipioSite);
+    }
+  });
+
+  context.subscriptions.push(configureSearch);
+  context.subscriptions.push(searchBySelection);
+  context.subscriptions.push(searchWithPrompt);
+}
