996ed29db69d9ee6f88c4cab280a161fd9e42cfa
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-10-19 16:39:05 +0100

    Refactoring of code

--- src/extension.ts
+++ src/extension.ts
@@ -3,102 +3,89 @@ import { URL } from 'url';
 
 import open = require('open');
 
-export function activate(context: vscode.ExtensionContext) {
-  /**
-   * Checks the urlstring is a valid Percipio URL
-   * it must be https:// and must end .percipio.com
-   *
-   * @param {string} [urlstring]
-   * @returns {boolean}
-   */
-  const isValidPercipioUrl = (urlstring?: string): boolean => {
-    if (!urlstring) {
-      return false;
-    }
-    let parsedurl;
 
-    try {
-      parsedurl = new URL(urlstring);
-    } catch (_) {
-      return false;
-    }
+/**
+ * Gets the selected text from the active editor
+ *
+ * @returns {string}
+ */
+function getSelectedText(): string {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return '';
+  }
 
-    // Check
-    return (parsedurl.protocol === 'https:' && parsedurl.hostname.toLowerCase().endsWith('.percipio.com'));
-  };
+  const { document } = editor;
+  const eol = document.eol === 1 ? '\n' : '\r\n';
 
-  /**
-   * Gets the selected text from the active editor
-   *
-   * @returns {string}
-   */
-  const getSelectedText = (): string => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return '';
+  const selectedTextLines = editor.selections.map((selection) => {
+    if (
+      selection.start.line === selection.end.line
+      && selection.start.character === selection.end.character
+    ) {
+      const { range } = document.lineAt(selection.start);
+      const text = editor.document.getText(range);
+      return `${text}${eol}`;
     }
 
-    const { document } = editor;
-    const eol = document.eol === 1 ? '\n' : '\r\n';
+    return editor.document.getText(selection);
+  });
 
-    const selectedTextLines = editor.selections.map((selection) => {
-      if (
-        selection.start.line === selection.end.line
-        && selection.start.character === selection.end.character
-      ) {
-        const { range } = document.lineAt(selection.start);
-        const text = editor.document.getText(range);
-        return `${text}${eol}`;
+  let result = '';
+  if (selectedTextLines.length > 0) {
+    result = selectedTextLines[0].trim();
+  }
+
+  return result;
+}
+
+/**
+ * Get the current language of the editor
+ *
+ * @returns {string}
+ */
+function getLanguageId(): string {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return '';
+  }
+  const { document } = editor;
+  return document.languageId;
+}
+
+/**
+ * Opens a new browser window to the Percipio site with the searchterm
+ *
+ * @param {string} searchTerm
+ * @returns {void}
+ */
+function executeSearch(searchTerm: string): void {
+  if (!searchTerm || searchTerm.trim() === '') {
+    return;
+  }
+
+  const currentSite = vscode.workspace.getConfiguration('percipioSearch').get<string>('siteUrl');
+  if (!currentSite || currentSite.trim() === '') {
+    vscode.window.showInformationMessage('You need to configure the Percipio Site', 'Open Settings').then(function (button) {
+      if (button === 'Open Settings') {
+        vscode.commands.executeCommand('workbench.action.openSettings', 'percipioSearch');
       }
-
-      return editor.document.getText(selection);
     });
+    return;
+  }
 
-    let result = '';
-    if (selectedTextLines.length > 0) {
-      result = selectedTextLines[0].trim();
-    }
+  const language = getLanguageId();
+  const search = language !== '' ? `"${searchTerm.trim()}" ${language}` : searchTerm.trim();
+  const encodedWebSearchTerm = encodeURIComponent(search);
+  const percipioSearchUrl = `${currentSite}/search?q=${encodedWebSearchTerm}`;
 
-    return result;
-  };
+  open(percipioSearchUrl);
+}
 
-  /**
-   * Get the current language of the editor
-   *
-   * @returns {string}
-   */
-  const getLanguageId = (): string => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return '';
-    }
-    const { document } = editor;
-    return document.languageId;
-  };
 
-  /**
-   * Opens a new browser window to the Percipio site with the searchterm
-   *
-   * @param {string} searchTerm
-   * @returns {void}
-   */
-  const executeSearch = (searchTerm: string): void => {
-    if (!searchTerm || searchTerm.trim() === '') {
-      return;
-    }
 
-    const currentSite = vscode.workspace.getConfiguration('percipioSearch').get<string>('siteUrl');
-    if (!currentSite || currentSite.trim() === '') {
-      return;
-    }
+export function activate(context: vscode.ExtensionContext) {
 
-    const language = getLanguageId();
-    const search = language !== '' ? `"${searchTerm.trim()}" ${language}` : searchTerm.trim();
-    const encodedWebSearchTerm = encodeURIComponent(search);
-    const percipioSearchUrl = `${currentSite}/search?q=${encodedWebSearchTerm}`;
-
-    open(percipioSearchUrl);
-  };
 
   const searchBySelection = vscode.commands.registerCommand(
     'extension.percipio-search-selection',
@@ -125,32 +112,21 @@ export function activate(context: vscode.ExtensionContext) {
     },
   );
 
-  const validatePercipioInput = (value: string): string => {
-    if (isValidPercipioUrl(value)) {
-      return '';
-    } else {
-      return 'Please enter a valid Percipio site url, such as https://demo.percipio.com';
-    }
-  };
-
   const configureSearch = vscode.commands.registerCommand('config.commands.configure-percipio-search', async () => {
-    const config = vscode.workspace.getConfiguration('percipioSearch');
-
-    const currentSite = config.get<string>('siteUrl');
-    // 1) Getting the Site Url
-    const percipioSite = await vscode.window.showInputBox({ placeHolder: 'Enter the Percipio Site URL', value: currentSite, validateInput: validatePercipioInput });
-
-    // Validate the url
-    if (percipioSite && isValidPercipioUrl(percipioSite) && currentSite !== percipioSite) {
-      // Clean the URL removing any path info etc
-      const myURL = new URL(percipioSite);
-
-      // 2) Update if different
-      config.update('siteUrl', myURL.origin, vscode.ConfigurationTarget.Global);
-    }
+    vscode.commands.executeCommand('workbench.action.openSettings', 'percipioSearch');
   });
 
   context.subscriptions.push(configureSearch);
   context.subscriptions.push(searchBySelection);
   context.subscriptions.push(searchWithPrompt);
+
+  const currentSite = vscode.workspace.getConfiguration('percipioSearch').get<string>('siteUrl');
+  if (!currentSite || currentSite.trim() === '') {
+    vscode.window.showInformationMessage('You need to configure the Percipio Site', 'Open Settings').then(function (button) {
+      if (button === 'Open Settings') {
+        vscode.commands.executeCommand('workbench.action.openSettings', 'percipioSearch');
+      }
+    });
+  }
+
 }
